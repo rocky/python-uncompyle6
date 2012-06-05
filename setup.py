b2a0b7201d8776b639b9cf3087e943dfe83f4eaf
Mysterie <kajusska@gmail.com>
2012-06-05 11:37:42 +0200

    MAJ owner/information

--- setup.py
+++ setup.py
@@ -7,9 +7,9 @@ from distutils.core import setup, Extension
 setup (name = "uncompyle2",
        version = "1.1",
        description = "Python byte-code to source-code converter",
-       author = "Hartmut Goebel",
-       author_email = "hartmut@oberon.noris.de",
-       url = "http://github.com/sysfrog/uncompyle",
+       author = "Mysterie",
+       author_email = "kajusska@gmail.com",
+       url = "http://github.com/Mysterie/uncompyle2",
        packages=['uncompyle2', 'uncompyle2.opcode'],
        scripts=['scripts/uncompyle2'],
       )
