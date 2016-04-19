1b71d0a0490d844bda99c54cfd81eeb168dc5af0
rocky <rocky@gnu.org>
2016-04-19 03:21:13 -0400

    Get ready for release 2.2.0

--- __pkginfo__.py
+++ __pkginfo__.py
@@ -47,7 +47,7 @@ def get_srcdir():
     return os.path.realpath(filename)
 
 ns = {}
-version            = '2.1.3'
+version            = '2.2.0'
 web                = 'https://github.com/rocky/python-uncompyle6/'
 
 # tracebacks in zip files are funky and not debuggable
