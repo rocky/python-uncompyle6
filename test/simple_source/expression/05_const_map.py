4455b5e280e8805f61bcab30780426c249d3a33f
rocky <rocky@gnu.org>
2016-05-01 21:06:52 -0400

    Add test for last fix.
    Drop 2.5 test until we figure out what's wrong

---
+++ test/simple_source/expression/05_const_map.py
@@ -0,0 +1,7 @@
+# Addresses a bug in the way Python 3.5+ handles
+# creation of map constants
+opts = {'highlight': True,
+            'start_line': -1,
+            'end_line': None
+            }
+print(opts)
