e1a2860013ae64e727635efa997ad2bb3901a5aa
rocky <rocky@gnu.org>
2016-05-13 16:25:07 -0400

    Test for class decorator
    See https://github.com/rocky/python-uncompyle6/pull/15

---
+++ test/simple_source/def/10_classdec.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# See https://github.com/rocky/python-uncompyle6/pull/15
+
+# In Python 2.7, you should see
+#   mkfuncdeco0 ::= mkfunc
+#   classdefdeco2 ::= LOAD_CONST expr mkfunc CALL_FUNCTION_0 BUILD_CLASS
+#   classdefdeco1 ::= expr classdefdeco1 CALL_FUNCTION_1
+#   designator ::= STORE_NAME
+#   classdefdeco ::= classdefdeco1 designator
+
+def author(*author_names):
+    def author_func(cls):
+        return cls
+    return author_func
+
+@author('Me', 'Him')
+@author('You')
+class MyClass(object):
+    def __init__(self):
+        pass
+
+    @staticmethod
+    @staticmethod
+    def static_method():
+        pass
+
+x = MyClass()
