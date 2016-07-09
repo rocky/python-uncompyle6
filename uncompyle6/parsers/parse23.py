047c95a1e57e5aeec71fed3a1f0da783790b0c2f
rocky <rb@dustyfeet.com>
2016-07-09 13:13:10 -0400

    2.3 while1stmt bug

--- uncompyle6/parsers/parse23.py
+++ uncompyle6/parsers/parse23.py
@@ -4,13 +4,20 @@
 
 from spark_parser import DEFAULT_DEBUG as PARSER_DEFAULT_DEBUG
 from uncompyle6.parser import PythonParserSingle
-from uncompyle6.parsers.parse2 import Python2Parser
+from uncompyle6.parsers.parse24 import Python24Parser
 
 class Python23Parser(Python24Parser):
 
     def __init__(self, debug_parser=PARSER_DEFAULT_DEBUG):
         super(Python24Parser, self).__init__(debug_parser)
         self.customized = {}
+
+    def p_misc23(self, args):
+        '''
+        _while1test ::= JUMP_FORWARD JUMP_IF_FALSE POP_TOP COME_FROM
+
+        while1stmt ::= SETUP_LOOP _while1test l_stmts JUMP_BACK COME_FROM POP_TOP POP_BLOCK COME_FROM
+        '''
 
 class Python23ParserSingle(Python23Parser, PythonParserSingle):
     pass
