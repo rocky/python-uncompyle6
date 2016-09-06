60b25f75969728a1ab035df6994fb14cd116dcbf
rocky <rb@dustyfeet.com>
2016-09-05 22:33:59 -0400

    Python 3.x bug in getting parameter of ** argument

--- test/simple_source/bug33/02_named_and_kwargs.py
+++ test/simple_source/bug33/02_named_and_kwargs.py
@@ -2,3 +2,11 @@
 # Bug is getting params correct: timeout before **kwargs
 def call(*popenargs, timeout=None, **kwargs):
     return
+
+# From 3.4 asyncio/base_events.py
+# Bug was in pickin up name of ** (kwargs)
+def subprocess_shell(self, protocol_factory, cmd, *, stdin=subprocess.PIPE,
+                     stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                     universal_newlines=False, shell=True, bufsize=0,
+                     **kwargs):
+    return
