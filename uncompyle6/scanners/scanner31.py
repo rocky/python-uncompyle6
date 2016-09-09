251de4338aeb629216520d2b8699b871efd39918
rocky <rb@dustyfeet.com>
2016-09-09 18:23:43 -0400

    3.1 scanner small fixes

--- uncompyle6/scanners/scanner31.py
+++ uncompyle6/scanners/scanner31.py
@@ -1,15 +1,15 @@
-#  Copyright (c) 2015-2016 by Rocky Bernstein
+#  Copyright (c) 2016 by Rocky Bernstein
 """
-Python 3.2 bytecode scanner/deparser
+Python 3.1 bytecode scanner/deparser
 
-This sets up opcodes Python's 3.2 and calls a generalized
+This sets up opcodes Python's 3.1 and calls a generalized
 scanner routine for Python 3.
 """
 
 from __future__ import print_function
 
 # bytecode verification, verify(), uses JUMP_OPs from here
-from xdis.opcodes import opcode_32 as opc
+from xdis.opcodes import opcode_31 as opc
 JUMP_OPs = map(lambda op: opc.opname[op], opc.hasjrel + opc.hasjabs)
 
 from uncompyle6.scanners.scanner3 import Scanner3
@@ -25,7 +25,7 @@ if __name__ == "__main__":
     if PYTHON_VERSION == 3.1:
         import inspect
         co = inspect.currentframe().f_code
-        tokens, customize = Scanner32().ingest(co)
+        tokens, customize = Scanner31().ingest(co)
         for t in tokens:
             print(t)
         pass
