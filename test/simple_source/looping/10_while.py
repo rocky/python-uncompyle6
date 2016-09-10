71591152efeea67b82ea570d418b490e5a3450d3
rocky <rb@dustyfeet.com>
2016-09-09 22:27:13 -0400

    3.0 .. 3.4 bug in whileTrue

--- test/simple_source/looping/10_while.py
+++ test/simple_source/looping/10_while.py
@@ -1,2 +1,13 @@
+# From 3.2 text_file.py
+def readline(self, line):
+    while True:
+        if self.join_lines:
+            if line:
+                continue
+            if self:
+                continue
+
+        return line
+
 while __name__ != '__main__':
     b = 4
