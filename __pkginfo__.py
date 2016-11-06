7343575e5526f1d8ebefb4901f8f8dbf2d181216
rocky <rb@dustyfeet.com>
2016-11-06 18:01:03 -0500

    Bump xdis to get correct 3.0 bytecodes

--- __pkginfo__.py
+++ __pkginfo__.py
@@ -38,7 +38,7 @@ entry_points={
     ]}
 ftp_url            = None
 install_requires   = ['spark-parser >= 1.4.0, < 1.5.0',
-                      'xdis >= 3.2.2, < 3.3.0']
+                      'xdis >= 3.2.3, < 3.3.0']
 license            = 'MIT'
 mailing_list       = 'python-debugger@googlegroups.com'
 modname            = 'uncompyle6'
