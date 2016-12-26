eb64a03dfa2a971886440d1c15370ac5f040d0bf
jiangpch <jiangpch@gohighsec.com>
2016-12-26 19:00:40 +0800

    add testcases for tuple assignment

--- test/simple_source/expression/03_tuple_assign.py
+++ test/simple_source/expression/03_tuple_assign.py
@@ -5,3 +5,8 @@ def some_function():
 def some_other_function():
     some_variable, = some_function()
     print(some_variable)
+
+empty_tup = ()
+one_item_tup = ("item1", )
+one_item_tup_without_parentheses = "item", 
+many_items_tup = ("item1", "item2", "item3")
