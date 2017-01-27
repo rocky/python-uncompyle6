41d63a026181925d67da5156ccec0166aaad08b4
rocky <rb@dustyfeet.com>
2017-01-27 16:41:31 -0500

    Bump min spark_parser version

--- __pkginfo__.py
+++ __pkginfo__.py
@@ -39,7 +39,7 @@ entry_points={
         'pydisassemble=uncompyle6.bin.pydisassemble:main',
     ]}
 ftp_url            = None
-install_requires   = ['spark-parser >= 1.5.1, < 1.6.0',
+install_requires   = ['spark-parser >= 1.5.2, < 1.6.0',
                       'xdis >= 3.2.4, < 3.3.0']
 license            = 'MIT'
 mailing_list       = 'python-debugger@googlegroups.com'
