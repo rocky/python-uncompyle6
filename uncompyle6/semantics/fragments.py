9a3e11a957dba7a0c87675a678707594fe0bd5e5
rocky <rb@dustyfeet.com>
2017-06-03 05:18:40 -0400

    Fragment bugs
    fragment.py:
    * deparse_code_aorund_offset: was sometimes returning the wrong type
    * capture function name offset
    * lint imports
    
    pysource.py: use a clearer variable name

--- uncompyle6/semantics/fragments.py
+++ uncompyle6/semantics/fragments.py
@@ -55,39 +55,26 @@ from __future__ import print_function
 
 import re, sys
 
-from uncompyle6 import PYTHON3, IS_PYPY
 from xdis.code import iscode
 from uncompyle6.semantics import pysource
 from uncompyle6 import parser
 from uncompyle6.scanner import Token, Code, get_scanner
 from uncompyle6.semantics.check_ast import checker
-from uncompyle6.semantics.helper import print_docstring
 
 from uncompyle6.show import (
     maybe_show_asm,
     maybe_show_ast,
-    maybe_show_ast_param_default,
 )
 
 from uncompyle6.parsers.astnode import AST
 
 from uncompyle6.semantics.pysource import (
-    ParserError, find_globals, StringIO)
+    ParserError, StringIO)
 
 from uncompyle6.semantics.consts import (
     INDENT_PER_LEVEL, NONE, PRECEDENCE,
     TABLE_DIRECT, escape, minint, MAP
     )
-
-from uncompyle6.semantics.make_function import (
-    find_all_globals, find_none, code_has_star_arg, code_has_star_star_arg
-)
-
-if PYTHON3:
-    from itertools import zip_longest
-else:
-    from itertools import izip_longest as zip_longest
-
 
 from spark_parser import DEFAULT_DEBUG as PARSER_DEFAULT_DEBUG
 
@@ -497,17 +484,20 @@ class FragmentsWalker(pysource.SourceWalker, object):
             # LOAD_CONST code object ..
             # LOAD_CONST        'x0'  if >= 3.3
             # MAKE_FUNCTION ..
-            code_index = -3
+            code_node = node[-3]
+        elif node[-2] == 'expr':
+            code_node = node[-2][0]
         else:
             # LOAD_CONST code object ..
             # MAKE_FUNCTION ..
-            code_index = -2
-        code = node[code_index]
-        func_name = code.attr.co_name
+            code_node = node[-2]
+        func_name = code_node.attr.co_name
         self.write(func_name)
+        self.set_pos_info(code_node, start, len(self.f.getvalue()))
 
         self.indentMore()
-        self.make_function(node, isLambda=False, codeNode=code)
+        start = len(self.f.getvalue())
+        self.make_function(node, isLambda=False, codeNode=code_node)
 
         self.set_pos_info(node, start, len(self.f.getvalue()))
 
@@ -1688,7 +1678,7 @@ def deparse_code_around_offset(name, offset, version, co, out=StringIO(),
     deparsed = deparse_code(version, co, out, showasm, showast, showgrammar, is_pypy)
     if (name, offset) in deparsed.offsets.keys():
         # This is the easy case
-        return deparsed.offsets[name, offset]
+        return deparsed
 
     valid_offsets = [t for t in deparsed.offsets if isinstance(t[1], int)]
     offset_list = sorted([t[1] for t in valid_offsets if t[0] == name])
