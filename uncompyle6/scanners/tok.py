9b336251a7edd5cc03c95a6e2f462e27ff76b845
rocky <rb@dustyfeet.com>
2017-09-09 07:47:21 -0400

    New-style Python classes only, please.

--- uncompyle6/scanners/tok.py
+++ uncompyle6/scanners/tok.py
@@ -1,4 +1,4 @@
-#  Copyright (c) 2016 by Rocky Bernstein
+#  Copyright (c) 2016-2017 by Rocky Bernstein
 #  Copyright (c) 2000-2002 by hartmut Goebel <h.goebel@crazy-compilers.com>
 #  Copyright (c) 1999 John Aycock
 
@@ -8,7 +8,7 @@ from uncompyle6 import PYTHON3
 if PYTHON3:
     intern = sys.intern
 
-class Token:
+class Token():
     """
     Class representing a byte-code instruction.
 
