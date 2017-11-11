4b0892bcb5fdad6d1eb553bbc20d9ca66d5fa8e5
rocky <rb@dustyfeet.com>
2017-11-10 22:30:03 -0500

    Use newer xdis

--- __pkginfo__.py
+++ __pkginfo__.py
@@ -40,7 +40,7 @@ entry_points       = {
     ]}
 ftp_url            = None
 install_requires   = ['spark-parser >= 1.7.1, < 1.8.0',
-                      'xdis >= 3.6.0, < 3.7.0', 'six']
+                      'xdis >= 3.6.1, < 3.7.0', 'six']
 license            = 'MIT'
 mailing_list       = 'python-debugger@googlegroups.com'
 modname            = 'uncompyle6'
