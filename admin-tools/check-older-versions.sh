35e4e034686065609b8b2e55cd37c9a391e16c00
rocky <rb@dustyfeet.com>
2017-11-13 09:53:10 -0500

    Administrivia

--- admin-tools/check-older-versions.sh
+++ admin-tools/check-older-versions.sh
@@ -13,6 +13,8 @@ if ! source ./setup-python-2.4.sh ; then
     exit $?
 fi
 
+PYVERSIONS='2.7.14 2.6.9 3.3.6 3.4.2 3.5.4 3.6.3'
+
 cd ..
 for version in $PYVERSIONS; do
     if ! pyenv local $version ; then
