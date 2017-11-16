f1004e64455fd6bb9be0f84fdec87c5da76fa682
rocky <rb@dustyfeet.com>
2017-11-16 11:23:04 -0500

    Bump copyright

--- uncompyle6/parsers/parse2.py
+++ uncompyle6/parsers/parse2.py
@@ -1,4 +1,4 @@
-#  Copyright (c) 2015-2016 Rocky Bernstein
+#  Copyright (c) 2015-2017 Rocky Bernstein
 #  Copyright (c) 2000-2002 by hartmut Goebel <h.goebel@crazy-compilers.com>
 #  Copyright (c) 1999 John Aycock
 """
