70ddd71c0eb9bf0c8d019388b5c225e863cdcfb4
rocky <rb@dustyfeet.com>
2018-01-12 22:25:42 -0500

    Test for 2.7 loop try else bug

---
+++ test/simple_source/bug27+/04_loop_try_else.py
@@ -0,0 +1,12 @@
+# From Python 2.7 ihooks.py
+def ensure_fromlist(self, fromlist):
+    for sub in fromlist:
+        if sub:
+            if not recursive:
+                try:
+                    all = 5
+                except AttributeError:
+                    pass
+                else:
+                    all = 6
+            continue
