38dffa32901c59e556a2a10351c86d529e2f2e41
rocky <rb@dustyfeet.com>
2018-03-07 08:00:36 -0500

    Allow setting max tests via MAX_TESTS env var

--- test/run-and-email.sh
+++ test/run-and-email.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 EMAIL=${EMAIL:-rb@dustyfeet.com}
+MAX_TESTS=${MAX_TESTS:-800}
 for VERSION in 2.7.14 2.6.9 ; do
     typeset -i rc=0
     LOGFILE=/tmp/pyenlib-$VERSION-$$.log
     if ! pyenv local $VERSION ; then
 	rc=1
     else
-      python ./test_pyenvlib.py --max 800 --weak-verify --$VERSION  >$LOGFILE 2>&1
+      python ./test_pyenvlib.py --max ${MAX_TESTS} --weak-verify --$VERSION  >$LOGFILE 2>&1
       rc=$?
     fi
     if ((rc == 0)); then
