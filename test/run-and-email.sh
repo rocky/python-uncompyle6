c8747cc89910601adcde30042c882abc36c8c0b4
rocky <rb@dustyfeet.com>
2018-03-07 07:52:21 -0500

    Only run if pyenv works

--- test/run-and-email.sh
+++ test/run-and-email.sh
@@ -1,10 +1,14 @@
-#!/usr/bin/bash
+#!/bin/bash
 EMAIL=${EMAIL:-rb@dustyfeet.com}
 for VERSION in 2.7.14 2.6.9 ; do
-    pyenv local $VERSION
+    typeset -i rc=0
     LOGFILE=/tmp/pyenlib-$VERSION-$$.log
-    python ./test_pyenvlib.py --max 800 --weak-verify --$VERSION  >$LOGFILE 2>&1
-    rc=$?
+    if ! pyenv local $VERSION ; then
+	rc=1
+    else
+      python ./test_pyenvlib.py --max 800 --weak-verify --$VERSION  >$LOGFILE 2>&1
+      rc=$?
+    fi
     if ((rc == 0)); then
 	tail -v $LOGFILE | mail -s \""$VERSION ok"\" rocky@localhost
     else
--- test/stdlib/run-and-email.sh
+++ test/stdlib/run-and-email.sh
@@ -1,8 +1,14 @@
-#!/usr/bin/bash
+#!/bin/bash
 EMAIL=${EMAIL:-rb@dustyfeet.com}
 for VERSION in 2.7.14 2.6.9 ; do
+    typeset -i rc=0
     LOGFILE=/tmp/runtests-$VERSION-$$.log
-    ./runtests.sh  >$LOGFILE 2>&1
+    if ! pyenv local $VERSION ; then
+	rc=1
+    else
+      ./runtests.sh  >$LOGFILE 2>&1
+      rc=$?
+    fi
     rc=$?
     if ((rc == 0)); then
 	tail -v $LOGFILE | mail -s \""$VERSION ok"\" rocky@localhost
