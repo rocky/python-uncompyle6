aa4416571be1f777c49c8ae77d18d65419ab2b8e
rocky <rocky@gnu.org>
2018-03-27 14:21:28 -0400

    grammar-cover run-and-email fixup

--- test/grammar-cover/run-and-email.sh
+++ test/grammar-cover/run-and-email.sh
@@ -2,6 +2,7 @@
 USER=${USER:-rocky}
 EMAIL=${EMAIL:-rb@dustyfeet.com}
 SUBJECT_PREFIX="grammar cover testing for"
+LOGFILE=/tmp/grammar-cover-$$.log
 /bin/bash ./grammar-all.sh  >$LOGFILE 2>&1
 rc=$?
 if ((rc == 0)); then
@@ -10,4 +11,3 @@ else
     tail -v $LOGFILE | mail -s "$SUBJECT_PREFIX not ok" ${USER}@localhost
     tail -v $LOGFILE | mail -s "$SUBJECT_PREFIX not ok" $EMAIL
 fi
-done
