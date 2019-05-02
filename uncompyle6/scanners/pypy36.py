e2c5a79346625922dbfa584edac9dcc0db342745
rocky <rb@dustyfeet.com>
2019-05-02 06:46:07 -0400

    Add pypy3.6 scanner

---
+++ uncompyle6/scanners/pypy36.py
@@ -0,0 +1,22 @@
+#  Copyright (c) 2019 by Rocky Bernstein
+"""
+Python PyPy 3.6 decompiler scanner.
+
+Does some additional massaging of xdis-disassembled instructions to
+make things easier for decompilation.
+"""
+
+import uncompyle6.scanners.scanner36 as scan
+
+# bytecode verification, verify(), uses JUMP_OPS from here
+from xdis.opcodes import opcode_35 as opc  # is this right?
+JUMP_OPs = opc.JUMP_OPS
+
+# We base this off of 3.5
+class ScannerPyPy36(scan.Scanner36):
+    def __init__(self, show_asm):
+        # There are no differences in initialization between
+        # pypy 3.6 and 3.6
+        scan.Scanner36.__init__(self, show_asm, is_pypy=True)
+        self.version = 3.6
+        return
