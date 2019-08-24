f8bfde4a8ee05350ae23c82766567f8e64e5ae97
rocky <rb@dustyfeet.com>
2019-08-24 07:47:48 -0400

    Get ready for release 3.4.0

--- test/simple_source/bug26/00_generator.py
+++
@@ -1,7 +0,0 @@
-# Issue #283 in Python 2.6
-# See https://github.com/rocky/python-uncompyle6/issues/283
-
-# This code is RUNNABLE!
-
-G = ( c for c in "spam, Spam, SPAM!" if c > 'A' and c < 'S')
-assert list(G) == ["P", "M"]
