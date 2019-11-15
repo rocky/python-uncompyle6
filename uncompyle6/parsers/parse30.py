1b4335edf1ed224fde55e2affc6742c0aea66e27
rocky <rb@dustyfeet.com>
2019-11-15 07:48:23 -0500

    Add 3.0.1 while loop rule

--- uncompyle6/parsers/parse30.py
+++ uncompyle6/parsers/parse30.py
@@ -157,7 +157,8 @@ class Python30Parser(Python31Parser):
                            JUMP_BACK come_froms POP_TOP POP_BLOCK COME_FROM_LOOP
         whilestmt      ::= SETUP_LOOP testexpr returns
                            POP_TOP POP_BLOCK COME_FROM_LOOP
-
+        whilestmt      ::= SETUP_LOOP testexpr l_stmts_opt come_from_opt
+                           come_froms POP_TOP POP_BLOCK COME_FROM_LOOP
 
 
         # compare_chained is like x <= y <= z
