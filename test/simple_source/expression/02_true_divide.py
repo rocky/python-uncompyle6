ca7f483dbb1cfbe03fb84661049d5730c13353a4
rocky <rb@dustyfeet.com>
2019-11-18 18:10:58 -0500

    Better test coverage of operators (in 2.7 and 3.0)

--- test/simple_source/expression/02_true_divide.py
+++ test/simple_source/expression/02_true_divide.py
@@ -1,4 +1,4 @@
 # Tests BINARY_TRUE_DIVIDE and INPLACE_TRUE_DIVIDE
-from __future__ import division  # needed on 2.6 and 2.7
+from __future__ import division  # needed on 2.2 .. 2.7
 x = len(__file__) / 1
 x /= 1
