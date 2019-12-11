bc614cf3fbbf77e3e563b1b9d97694ea15bbe023
rocky <rb@dustyfeet.com>
2019-12-11 07:32:35 -0500

    Another 3.7+ "or" grammar rule

--- uncompyle6/parsers/parse37.py
+++ uncompyle6/parsers/parse37.py
@@ -954,6 +954,10 @@ class Python37Parser(Python37BaseParser):
         or   ::= and jitop_come_from expr COME_FROM
         or   ::= expr JUMP_IF_TRUE_OR_POP expr COME_FROM
         or   ::= expr JUMP_IF_TRUE expr COME_FROM
+
+        testfalse ::= or jmp_false COME_FROM
+        or        ::= expr jmp_true expr
+
         and  ::= expr JUMP_IF_FALSE_OR_POP expr COME_FROM
         and  ::= expr JUMP_IF_FALSE expr COME_FROM
 
