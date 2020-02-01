4dc64063d1332c56ad615eb0376c36bfc5c271e3
rocky <rb@dustyfeet.com>
2020-02-01 11:14:19 -0500

    Small change

--- uncompyle6/parsers/parse2.py
+++ uncompyle6/parsers/parse2.py
@@ -673,6 +673,7 @@ class Python2Parser(PythonParser):
         if fn:
             if fn(self, lhs, n, rule, ast, tokens, first, last):
                 return True
+            pass
         if rule == ("and", ("expr", "jmp_false", "expr", "\\e_come_from_opt")):
             # If the instruction after the instructions forming the "and"  is an "YIELD_VALUE"
             # then this is probably an "if" inside a comprehension.
