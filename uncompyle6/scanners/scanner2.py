f117feb58562e5c6370a8cec2b2e258c56eaf3a1
rocky <rb@dustyfeet.com>
2020-04-16 11:01:12 -0400

    Use new xdis...
    Sadly there are some regressions that need to be fixed.
    
    Deal with later.

--- uncompyle6/scanners/scanner2.py
+++ uncompyle6/scanners/scanner2.py
@@ -138,14 +138,21 @@ class Scanner2(Scanner):
         if classname:
             classname = '_' + classname.lstrip('_') + '__'
 
-            free = [ self.unmangle_name(name, classname)
+            if hasattr(co, "co_cellvars"):
+                free = [ self.unmangle_name(name, classname)
                      for name in (co.co_cellvars + co.co_freevars) ]
+            else:
+                free = ()
+
             names = [ self.unmangle_name(name, classname)
                       for name in co.co_names ]
             varnames = [ self.unmangle_name(name, classname)
                          for name in co.co_varnames ]
         else:
-            free = co.co_cellvars + co.co_freevars
+            if hasattr(co, "co_cellvars"):
+                free = co.co_cellvars + co.co_freevars
+            else:
+                free = ()
             names = co.co_names
             varnames = co.co_varnames
         return free, names, varnames
