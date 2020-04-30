a72163f6f9438691cb1fbfe0b3e636f0d1f90a6d
rocky <rb@dustyfeet.com>
2020-04-30 18:00:04 -0400

    lint

--- uncompyle6/semantics/make_function2.py
+++ uncompyle6/semantics/make_function2.py
@@ -18,9 +18,7 @@ All the crazy things we have to do to handle Python functions in Python before 3
 The saga of changes continues in 3.0 and above and in other files.
 """
 from xdis import iscode, code_has_star_arg, code_has_star_star_arg
-from xdis.util import CO_GENERATOR
 from uncompyle6.scanner import Code
-from uncompyle6.parsers.treenode import SyntaxTree
 from uncompyle6 import PYTHON3
 from uncompyle6.semantics.parser_error import ParserError
 from uncompyle6.parser import ParserError as ParserError2
