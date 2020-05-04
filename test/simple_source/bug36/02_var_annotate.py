d3cf87e2d9959bafb64ba8b1bcefc812722b5c78
rocky <rb@dustyfeet.com>
2020-05-04 11:43:16 -0400

    Start marking test suite since this is going to be copied

--- test/simple_source/bug36/02_var_annotate.py
+++ test/simple_source/bug36/02_var_annotate.py
@@ -1,6 +1,9 @@
 # 3.6+ type annotations on variables
 from typing import List
 
+# This test program is part of the uncompyle6 test suite
+# tests STORE_ANNOTATION and SETUP_ANOTATIONS
+
 # RUNNABLE!
 y = 2
 x: bool
