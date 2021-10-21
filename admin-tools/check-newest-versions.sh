162423895ec3bc68573f893cd9b5c19911c6ddde
rocky <rb@dustyfeet.com>
2021-10-21 02:31:59 -0400

    Administrivia

---
+++ admin-tools/check-newest-versions.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+function finish {
+  cd $owd
+}
+
+# FIXME put some of the below in a common routine
+owd=$(pwd)
+trap finish EXIT
+
+cd $(dirname ${BASH_SOURCE[0]})
+if ! source ./pyenv-newest-versions ; then
+    exit $?
+fi
+if ! source ./setup-master.sh ; then
+    exit $?
+fi
+cd ..
+for version in $PYVERSIONS; do
+    echo --- $version ---
+    if ! pyenv local $version ; then
+	exit $?
+    fi
+    make clean && pip install -e .
+    if ! make check-short; then
+	exit $?
+    fi
+    echo === $version ===
+done
+make check
