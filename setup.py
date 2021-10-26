3ad63071ac1b24698aa5698198f51f010b445c9b
rocky <rb@dustyfeet.com>
2021-10-26 18:25:30 -0400

    Admnistrivia: package info

--- setup.py
+++ setup.py
@@ -4,11 +4,16 @@ import sys
 """Setup script for the 'uncompyle6' distribution."""
 
 SYS_VERSION = sys.version_info[0:2]
-if not ((2, 6) <= SYS_VERSION <= (3, 9)):
-    mess = "Python Release 2.6 .. 3.9 are supported in this code branch."
+if not ((2, 4) <= SYS_VERSION < (3, 11)):
+    mess = "Python Release 2.6 .. 3.10 are supported in this code branch."
     if (2, 4) <= SYS_VERSION <= (2, 7):
         mess += (
             "\nFor your Python, version %s, use the python-2.4 code/branch."
+            % sys.version[0:3]
+        )
+    if (3, 3) <= SYS_VERSION < (3, 6):
+        mess += (
+            "\nFor your Python, version %s, use the python-3.3-to-3.5 code/branch."
             % sys.version[0:3]
         )
     elif SYS_VERSION < (2, 4):
