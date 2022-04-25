d8d8ed60d7b0da2953bf21a4e2c7933b6527aae5
rocky <rb@dustyfeet.com>
2022-04-25 07:56:41 -0400

    Python 3.3 tolerance

--- uncompyle6/parsers/parse37base.py
+++ uncompyle6/parsers/parse37base.py
@@ -321,18 +321,18 @@ class Python37BaseParser(PythonParser):
 
             elif opname in ("BUILD_CONST_LIST", "BUILD_CONST_DICT", "BUILD_CONST_SET"):
                 if opname == "BUILD_CONST_DICT":
-                    rule = f"""
+                    rule = """
                             add_consts          ::= ADD_VALUE*
-                            const_list          ::= COLLECTION_START add_consts {opname}
+                            const_list          ::= COLLECTION_START add_consts %s
                             dict                ::= const_list
                             expr                ::= dict
-                        """
+                        """ % opname
                 else:
-                    rule = f"""
+                    rule = """
                             add_consts          ::= ADD_VALUE*
-                            const_list          ::= COLLECTION_START add_consts {opname}
+                            const_list          ::= COLLECTION_START add_consts %s
                             expr                ::= const_list
-                        """
+                        """ % opname
                 self.addRule(rule, nop_func)
 
             elif opname_base == "BUILD_CONST_KEY_MAP":
