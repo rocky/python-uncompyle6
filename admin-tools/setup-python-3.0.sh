766618ba48fc8d87a81fbb8d3118926565b4dfb1
rocky <rb@dustyfeet.com>
2022-11-04 00:53:45 -0400

    Alow 3.0 setup

---
+++ admin-tools/setup-python-3.0.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+PYTHON_VERSION=3.0.1
+pyenv local $PYTHON_VERSION
+
+# FIXME put some of the below in a common routine
+function checkout_version {
+    local repo=$1
+    version=${2:-python-3.0-to-3.2}
+    echo Checking out $version on $repo ...
+    (cd ../$repo && git checkout $version && pyenv local $PYTHON_VERSION) && \
+	git pull
+    return $?
+}
+
+function finish {
+  cd $owd
+}
+
+export PATH=$HOME/.pyenv/bin/pyenv:$PATH
+owd=$(pwd)
+bs=${BASH_SOURCE[0]}
+if [[ $0 == $bs ]] ; then
+    echo "This script should be *sourced* rather than run directly through bash"
+    exit 1
+fi
+
+mydir=$(dirname $bs)
+fulldir=$(readlink -f $mydir)
+cd $fulldir/..
+(cd $fulldir/.. && checkout_version python-spark master && checkout_version python-xdis &&
+      checkout_version python-uncompyle6)
+cd $owd
+rm -v */.python-version || true
+
+git checkout python-3.0-to-3.2  && git pull && pyenv local $PYTHON_VERSION
