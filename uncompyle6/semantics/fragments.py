f50086f1dae8cca9397fb9e309eb7eae072a0c70
rocky <rb@dustyfeet.com>
2023-08-11 14:26:41 -0400

    Remove type annotation

--- uncompyle6/semantics/fragments.py
+++ uncompyle6/semantics/fragments.py
@@ -2154,7 +2154,7 @@ def code_deparse_around_offset(
     co,
     out=StringIO(),
     version=None,
-    is_pypy: bool = False,
+    is_pypy = False,
     debug_opts=DEFAULT_DEBUG_OPTS,
 ):
     """
