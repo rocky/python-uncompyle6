f7caf9b675a8a0fc7dfb89c9fcc8dfbe027d2cbd
rocky <rb@dustyfeet.com>
2024-02-03 15:15:55 -0500

    Remove strayh blank line

--- uncompyle6/semantics/linemap.py
+++ uncompyle6/semantics/linemap.py
@@ -1,6 +1,5 @@
 #  Copyright (c) 2018, 2024 by Rocky Bernstein
 #
-
 #  This program is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation, either version 3 of the License, or
