d9ff58391f74110ef026d7d9ae380ba0505b1293
rocky <rb@dustyfeet.com>
2024-02-25 06:09:01 -0500

    Admnistrivia

--- admin-tools/merge-for-2.4.sh
+++ admin-tools/merge-for-2.4.sh
@@ -1,5 +1,7 @@
 #/bin/bash
+owd=$(pwd)
 cd $(dirname ${BASH_SOURCE[0]})
 if . ./setup-python-2.4.sh; then
     git merge python-3.0-to-3.2
 fi
+cd $owd
--- admin-tools/merge-for-3.0.sh
+++ admin-tools/merge-for-3.0.sh
@@ -1,5 +1,7 @@
 #/bin/bash
+owd=$(pwd)
 cd $(dirname ${BASH_SOURCE[0]})
 if . ./setup-python-3.0.sh; then
     git merge python-3.3-to-3.5
 fi
+cd $owd
