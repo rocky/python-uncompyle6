30463f75d6e2c1d92417b27d4e9ab3c68f0c9021
rocky <rb@dustyfeet.com>
2024-03-02 05:38:38 -0500

    2.4 tolerance (remove annotations)

--- uncompyle6/parsers/reducecheck/pop_return.py
+++ uncompyle6/parsers/reducecheck/pop_return.py
@@ -1,9 +1,7 @@
 #  Copyright (c) 2020 Rocky Bernstein
 
 
-def pop_return_check(
-    self, lhs: str, n: int, rule, ast, tokens: list, first: int, last: int
-) -> bool:
+def pop_return_check(self, lhs, n, rule, ast, tokens, first, last):
     # If the first instruction of return_expr (the instruction after POP_TOP) is
     # has a linestart, then the POP_TOP was probably part of the previous
     # statement, such as a call() where the return value is discarded.
