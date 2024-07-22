f82caba70f8f84c4e43a1067b89f9a5659840206
rocky <rb@dustyfeet.com>
2024-07-22 18:03:17 -0400

    Administrivia

--- admin-tools/setup-master.sh
+++ admin-tools/setup-master.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 # Check out master branch and dependent development master branches
-PYTHON_VERSION=3.8
-
 bs=${BASH_SOURCE[0]}
 if [[ $0 == $bs ]] ; then
     echo "This script should be *sourced* rather than run directly through bash"
     exit 1
 fi
 
-mydir=$(dirname $bs)
+PYTHON_VERSION=3.8
+
 uncompyle6_owd=$(pwd)
+mydir=$(dirname $bs)
+fulldir=$(readlink -f $mydir)
 cd $mydir
 . ./checkout_common.sh
-fulldir=$(readlink -f $mydir)
 cd $fulldir/..
 (cd $fulldir/.. && \
      setup_version python-spark master && \
--- admin-tools/setup-python-3.0.sh
+++ admin-tools/setup-python-3.0.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 # Check out python-3.0-to-3.2 and dependent development branches.
-
-PYTHON_VERSION=3.0.1
-
 bs=${BASH_SOURCE[0]}
 if [[ $0 == $bs ]] ; then
     echo "This script should be *sourced* rather than run directly through bash"
     exit 1
 fi
 
+PYTHON_VERSION=3.0.1
+
+uncompyle6_owd=$(pwd)
 mydir=$(dirname $bs)
-. ./checkout_common.sh
 fulldir=$(readlink -f $mydir)
-cd $fulldir
-cd $fulldir/..
+cd $mydir
+. ./checkout_common.sh
 (cd $fulldir/.. && \
      setup_version python-spark master && \
-     checkout_version python-xdis python-3.0)
+     setup_version python-xdis python-3.0)
 
 checkout_finish python-3.0-to-3.2
