193c262ffbf03dd0d29b5095fb7f604e9bbf047a
rocky <rb@dustyfeet.com>
2024-10-09 03:55:30 -0400

    Administrivia
    Folloow branching of other projects in this ilk

--- admin-tools/setup-python-3.0.sh
+++ admin-tools/setup-python-3.0.sh
@@ -6,7 +6,7 @@ if [[ $0 == $bs ]] ; then
     exit 1
 fi
 
-PYTHON_VERSION=3.0.1
+PYTHON_VERSION=3.0
 
 uncompyle6_owd=$(pwd)
 mydir=$(dirname $bs)
--- admin-tools/setup-python-3.6.sh
+++ admin-tools/setup-python-3.6.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
-# Check out master branch and dependent development master branches
+# Check out python-3.6-to-3.10 and dependent development branches.
 bs=${BASH_SOURCE[0]}
 if [[ $0 == $bs ]] ; then
     echo "This script should be *sourced* rather than run directly through bash"
     exit 1
 fi
 
-PYTHON_VERSION=3.12
+PYTHON_VERSION=3.6
 
 uncompyle6_owd=$(pwd)
 mydir=$(dirname $bs)
-fulldir=$(readlink -f $mydir)
 cd $mydir
+fulldir=$(readlink -f $mydir)
 . ./checkout_common.sh
 cd $fulldir/..
 (cd $fulldir/.. && \
-     setup_version python-spark master && \
-     setup_version python-xdis master )
-checkout_finish master
+     setup_version python-spark python-3.6 && \
+     setup_version python-xdis python-3.6 )
+
+checkout_finish python-3.6-to-3.10
